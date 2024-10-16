bd9bab812c5e092495aad21fcf151e49d13ccf23
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-15 18:53:39 -0700

    updated TimerScreen

--- app/timer.tsx
+++ app/timer.tsx
@@ -1,14 +1,27 @@
+import { View, StyleSheet } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
 export default function TimerScreen() {
   return (
-    <ThemedView>
-      <ThemedText type="title">Timer</ThemedText>
-      <ThemedText type="default">
-        This is where the timer page would be.
-      </ThemedText>
-    </ThemedView>
+    <View style={styles.container}>
+      <ThemedView>
+        <ThemedText type="title">Timer</ThemedText>
+      </ThemedView>
+      <ThemedView>
+        <ThemedText type="default">
+          This is where the timer page would be.
+        </ThemedText>
+      </ThemedView>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 50,
+  },
+});
