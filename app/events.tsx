a53062215482893e2ac799f54e3a49f2e5b3569a
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-15 18:55:55 -0700

    updated EventsScreen

--- app/events.tsx
+++ app/events.tsx
@@ -2,6 +2,7 @@ import { View, StyleSheet } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { styles } from "@/constants/styles";
 
 
 export default function EventsScreen() {
@@ -18,11 +19,3 @@ export default function EventsScreen() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    marginTop: 50,
-  },
-});
