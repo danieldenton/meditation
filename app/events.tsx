3086d0ad98a485c247441fe39605c71474a66c34
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-15 18:44:52 -0700

    events screen

---
+++ app/events.tsx
@@ -0,0 +1,16 @@
+
+
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+
+export default function EventsScreen() {
+  return (
+    <ThemedView>
+      <ThemedText type="title">Events</ThemedText>
+      <ThemedText type="default">
+        This is where the events page would be.
+      </ThemedText>
+    </ThemedView>
+  );
+}
