4b4859d7aece78eba9fcf7d24ac85d0e23852332
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-15 18:59:10 -0700

    updated FoodForNonThoughtScreen

--- app/food-for-non-thought.tsx
+++ app/food-for-non-thought.tsx
@@ -1,15 +1,21 @@
-
+import { View } from "react-native";
 
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { styles } from "@/constants/styles";
 
-export default function FoodForNonThoughtScreen() {
-    return (            
-        <ThemedView>
-            <ThemedText type="title">Food for Non-Thought</ThemedText>
-            <ThemedText type="default">
-                This is where the daily wisdom message would be.
-            </ThemedText>
-        </ThemedView>
-    )
+
+export default function EventsScreen() {
+  return (
+    <View style={styles.container}>
+    <ThemedView>
+      <ThemedText type="title">Food For Non-Thought</ThemedText>
+      </ThemedView>
+      <ThemedView>
+      <ThemedText type="default">
+        This is where the food for non-thought page would be.
+      </ThemedText>
+    </ThemedView>
+    </View>
+  );
 }
