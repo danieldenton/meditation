95c583494608fc69b801c9e09a77407e9853f57d
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-23 15:56:05 -0700

    firebase underway

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,26 @@
 import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import "react-native-reanimated";
+import firebase from "firebase/compat/app";
 
 import { UserContextProvider } from "@/context/user";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+// const firebaseConfig = {
+//     apiKey: "",
+//     authDomain: "",
+//     projectId: "meditation-app-7e00d",
+//     storageBucket: "",
+//     messagingSenderId: "",
+//     appId: "",
+//     measurementId: "",
+//   };
+  
+//   if (!firebase.apps.length) {
+//     firebase.initializeApp(firebaseConfig);
+//   }
 
 export default function Root() {
   return (
