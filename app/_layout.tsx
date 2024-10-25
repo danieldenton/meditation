080921b87f8b3177f8884bf226d6aa71fb14f062
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-25 10:29:23 -0700

    using expo for .env. updated FIREBASE env variables in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,27 +4,18 @@ import "react-native-reanimated";
 import { initializeApp } from "firebase/app";
 
 import { UserContextProvider } from "@/context/user";
-import {
-  FIREBASE_API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-  MEASUREMENT_ID,
-} from "@env";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_MEASUREMENT_ID,
 };
 
 initializeApp(firebaseConfig);
